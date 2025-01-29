7c8915b0b995bdecfe8c4e51817beb53d034dfda
KayoticCarnige <kayos156official@gmail.com>
2025-01-29 17:40:22 -0500

    Fixed a big bug
    Thanks to @d_iscord_made_me_do_it on the official Fabric server for pointing me in the right direction!

--- src/main/java/com/kckarnige/toolsofsteel/Toolsofsteel.java
+++ src/main/java/com/kckarnige/toolsofsteel/Toolsofsteel.java
@@ -5,18 +5,11 @@ import com.kckarnige.toolsofsteel.config.MidnightConfigStuff;
 import com.kckarnige.toolsofsteel.items.ItemRegister;
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static net.fabricmc.fabric.api.resource.ResourcePackActivationType.*;
 
 public class Toolsofsteel implements ModInitializer {
 
--- src/main/java/com/kckarnige/toolsofsteel/mixin/VanillaToolChanges.java
+++ src/main/java/com/kckarnige/toolsofsteel/mixin/VanillaToolChanges.java
@@ -1,20 +1,27 @@
 package com.kckarnige.toolsofsteel.mixin;
 
 import com.kckarnige.toolsofsteel.config.MidnightConfigStuff;
-import net.minecraft.item.*;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.recipe.Ingredient;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.lang.reflect.Field;
 import java.util.function.Supplier;
 
 import static com.kckarnige.toolsofsteel.items.ItemRegister.DIAMOND_INGOT;
 
 @Mixin(ToolMaterials.class)
 public class VanillaToolChanges {
+
+    @Mutable
+    @Final
+    @Shadow private int itemDurability;
+    @Mutable
+    @Final
+    @Shadow private Supplier<Ingredient> repairIngredient;
+
     @Inject(method = "<clinit>", at = @At("TAIL"))
     private static void modifyDiamondMaterial(CallbackInfo ci) {
         replaceDiamondMaterial();
@@ -22,38 +29,16 @@ public class VanillaToolChanges {
 
     @Unique
     private static void replaceDiamondMaterial() {
-        try {
-            // Access the material info
-            Field diamondField = ToolMaterials.class.getDeclaredField("DIAMOND");
-            Field netheriteField = ToolMaterials.class.getDeclaredField("NETHERITE");
-            diamondField.setAccessible(true);
-            netheriteField.setAccessible(true);
+        ToolMaterials diamondMaterial = ToolMaterials.DIAMOND;
+        ToolMaterials netheriteMaterial = ToolMaterials.NETHERITE;
 
-            // Make the material info editable
-            ToolMaterials diamondMaterial = (ToolMaterials) diamondField.get(null);
-            ToolMaterials netheriteMaterial = (ToolMaterials) netheriteField.get(null);
+        if (!MidnightConfigStuff.REMOVE_DURABILITY_CHANGES) {
+            ((VanillaToolChanges) (Object) diamondMaterial).itemDurability = 550;
+            ((VanillaToolChanges) (Object) netheriteMaterial).itemDurability = 650;
+        }
 
-
-
-            if (!MidnightConfigStuff.REMOVE_DURABILITY_CHANGES) {
-                // Access itemDurability variable in ToolMaterials class
-                Field durabilityField = ToolMaterials.class.getDeclaredField("itemDurability");
-                durabilityField.setAccessible(true);
-
-                durabilityField.set(diamondMaterial, 550);
-                durabilityField.set(netheriteMaterial, 650);
-            }
-
-            if (!MidnightConfigStuff.REMOVE_REPAIR_CHANGES) {
-                // Access repairIngredient variable in ToolMaterials class
-                Field repairIngredientField = ToolMaterials.class.getDeclaredField("repairIngredient");
-                repairIngredientField.setAccessible(true);
-
-                repairIngredientField.set(diamondMaterial, (Supplier<Ingredient>) () -> Ingredient.ofStacks(DIAMOND_INGOT.getDefaultStack()));
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException("[Tools of Steel] Failed to replace Diamond and Netherite tool durability", e);
+        if (!MidnightConfigStuff.REMOVE_REPAIR_CHANGES) {
+            ((VanillaToolChanges) (Object) diamondMaterial).repairIngredient = () -> Ingredient.ofStacks(DIAMOND_INGOT.getDefaultStack());
         }
     }
 }
