735856db847574f22343da976e026f58afba40bd
KayoticCarnige <kayos156official@gmail.com>
2025-01-31 22:34:00 -0500

    Another update?

--- src/main/java/com/kckarnige/toolsofsteel/client/ToolsofsteelClient.java
+++ src/main/java/com/kckarnige/toolsofsteel/client/ToolsofsteelClient.java
@@ -3,16 +3,17 @@ package com.kckarnige.toolsofsteel.client;
 import com.kckarnige.toolsofsteel.config.MidnightConfigStuff;
 import com.kckarnige.toolsofsteel.utils.PackRegister;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.minecraft.text.Text;
 
-import static net.fabricmc.fabric.api.resource.ResourcePackActivationType.DEFAULT_ENABLED;
 
 public class ToolsofsteelClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         if (!MidnightConfigStuff.REMOVE_RPS) {
-            PackRegister.registerPack("visual_tweaks", DEFAULT_ENABLED, Text.translatable("pack.toolsofsteel.visual_tweaks.name"));
+            PackRegister.registerPack("visual_tweaks", ResourcePackActivationType.DEFAULT_ENABLED, Text.translatable("pack.toolsofsteel.visual_tweaks.name"));
+            PackRegister.registerPack("consistent_helms", ResourcePackActivationType.NORMAL, Text.translatable("pack.toolsofsteel.consistent_helms.name"));
         }
     }
 }
